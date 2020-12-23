cddd6f9e521473aaa634b9709c60b8dbce884e54
Hypnocapybara <apolevki09@gmail.com>
2020-12-22 19:43:29 -0500

    Net class with feed_forward

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 from perceptron.neuron import Neuron
+from perceptron.net import Net
 
 
 def main():
-    weights = [0, 1]
-    bias = 0
+    # weights = [0, 1]
+    # bias = 0
+    #
+    # h1 = Neuron(weights, bias)
+    # h2 = Neuron(weights, bias)
+    # o1 = Neuron(weights, bias)
+    #
+    # out_h1 = h1.feed_forward([2, 3])
+    # out_h2 = h2.feed_forward([2, 3])
+    #
+    # out = o1.feed_forward([out_h1, out_h2])
+    # print(out)
 
-    h1 = Neuron(weights, bias)
-    h2 = Neuron(weights, bias)
-    o1 = Neuron(weights, bias)
-
-    out_h1 = h1.feed_forward([2, 3])
-    out_h2 = h2.feed_forward([2, 3])
-
-    out = o1.feed_forward([out_h1, out_h2])
-    print(out)
+    net = Net([2, 2, 1])
+    print(net.feed_forward([2, 3]))
 
 
 if __name__ == '__main__':
--- perceptron/net.py
+++ perceptron/net.py
@@ -1,6 +1,25 @@
+from typing import List
 
+from .neuron import Neuron
 
 
 class Net:
-    def __init__(self, structure):
-        pass
+    neurons: List[List[Neuron]] = []
+
+    def __init__(self, structure: List[int]):
+        assert len(structure) > 2
+
+        for i in range(1, len(structure)):
+            input_weights = [0.5] * structure[i-1]
+            layer = [Neuron(input_weights, 0) for _g in range(structure[i])]
+            self.neurons.append(layer)
+
+    def feed_forward(self, inputs: List[float]) -> List[float]:
+        assert len(self.neurons) > 0
+        assert len(inputs) == len(self.neurons[0])
+
+        next_inputs = inputs
+        for layer in self.neurons:
+            next_inputs = [layer[i].feed_forward(next_inputs) for i in range(len(layer))]
+
+        return next_inputs
